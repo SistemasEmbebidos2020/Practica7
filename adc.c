857f7a0e4681561c4417be24e1085394c8a0c34c
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-06-21 12:11:54 -0500

    Update adc.c

--- adc.c
+++ adc.c
@@ -8,13 +8,22 @@ ADMUX = 0B01000001;	//AVCC- AREF y ADC1
 ADCSRA = 0B00000111;	//PRE-SCALER  128 - DISABLED ANALOG CONVERTER & CONVERSION NOT STARTED
 ADCSRB = 0B00000000; 	//FREE RUNNING MODE
 DIDR0 = 0B00000010;	//ENABLE ANALOG MODE ADC1
+serial_begin(9600);
 while (1)
       {
 	 ADCSRA = 0B11000111; // ENABLED ANALOG CONVERTER & START CONVERSION
 	 while(ADCSRA & (1<< ADSC)); // ADSC DISABLED? 
-	 //ADCSRA = 0B00000111;
-	 PORTD = ADCL;
-	 PORTB = ADCH;
+	
+	/* PORTD = ADCL;
+	 PORTB = ADCH;*/ //simulado
+	
+	//presencial
+	float r = ADC/1023.0*5.0;
+	char rec1[20] ;
+	dtostrf(r, 2, 4, rec1);
+	serial_println_str(rec1);
+	
+	
 	 _delay_ms(200);
 }
    return 0;
