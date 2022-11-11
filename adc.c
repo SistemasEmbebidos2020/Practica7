7e6ef6499d3937791b62ea063115c965fb55d249
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-11-10 22:35:48 -0500

    Update adc.c

--- adc.c
+++ adc.c
@@ -3,6 +3,24 @@
 #include "uart.h"
 #include <stdlib.h>
 
+void mostrarleds(){
+	float r = ADC/1023.0*5.0;
+
+	 if (r<0.49) {PORTB = 0; PORTD = 0;}
+else if (r<1.00) {PORTB=1; PORTD &= ~0B11110000; }
+else if (r<1.50) {PORTB=3;  PORTD = 0;  }
+else if (r<2.00) {PORTB=7;  PORTD = 0;  }
+else if (r<2.50) {PORTB=15; PORTD = 0;  }
+else if (r<3.00) {PORTB=31; PORTD = 0;  }
+else if (r<3.50) {PORTB=63; PORTD = 0;  }
+//else if (r>=3.50) {PORTB=63; PORTD =  0X10 ;  }	//PARA USAR ESTO, SE REQUIERE REAZILAR LA CONEXION DE PULL UP EXTERNAMENTE
+else if (r<4.00) {PORTB=63; PORTD |=  0B00010000; PORTD &= ~(1<<5) ;  }
+else if (r<4.50) {PORTB=63; PORTD |=  0B00110000; PORTD &= ~(1<<6) ;  }
+else if (r<5.00) {PORTB=63; PORTD |=  0B01110000; PORTD &= ~(1<<7) ;  }
+else 			 {PORTB=63; PORTD |=  0B11110000;  }
+
+}
+
 int main()
  {
 
@@ -16,16 +34,8 @@ while (1)
 	 ADCSRA = 0B11000111; // ENABLED ANALOG CONVERTER & START CONVERSION
 	 while(ADCSRA & (1<< ADSC)); // ADSC DISABLED? 
 	
-	/* PORTD = ADCL;
-	 PORTB = ADCH;*/ //simulado
-	
 	//presencial
-	float r = ADC/1023.0*5.0;
-	char rec1[20] ;
-	dtostrf(r, 2, 4, rec1);
-	serial_println_str(rec1);
-	
-	
+	mostrarleds();
 	 _delay_ms(200);
 }
    return 0;
